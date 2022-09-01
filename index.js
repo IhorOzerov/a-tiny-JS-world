d5324c3ed5875d0af377529259225d446e78c064
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-09-01 15:42:06 +0300

    add my app

--- index.js
+++ index.js
@@ -6,10 +6,76 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 
+const man = {
+   creature: 'human',
+   name: "Gregory",
+   gender: 'male',
+   hands: 2,
+   legs: 2,
+   say: "Hello, woman!",
+   friends: ["Bandit", "Gangster"]
+};
+const woman = {
+   creature: 'human',
+   name: "Yohanna",
+   gender: 'female',
+   hands: 2,
+   legs: 2,
+   say: "Hello, man!",
+   friends:"Gangster"
+};
+const dog = {
+   creature: 'dog',
+   name: "Bandit",
+   gender: 'male',
+   hands: 0,
+   legs: 4,
+   say: "Bark!",
+   friends:  ["Gregory", "Yohanna"]
+};
+const cat = {
+   creature: 'cat',
+   name: "Gangster",
+   gender: 'male',
+   hands: 0,
+   legs: 4,
+   say: "Where is my money, Gregory?!",
+   friends:  ["Gregory", "Vanessa"]
+};
+const catWoman = Object.create(cat);
+catWoman.creature = "cat-woman";
+catWoman.name = "Vanessa";
+catWoman.gender = 'female';
+catWoman.legs = 2;
+catWoman.hands = 2;
+catWoman.friends =  ["Yohanna", "Gangster"];
 
 // ======== OUTPUT ========
+const keys = {
+   creature: "I am ",
+   name: "My name is ",
+   gender: "I am a pretty ",
+   hands: "I have a ",
+   legs: "hands and ",
+   friends: "legs I like to talk with ",
+   say: "I would like to say "
+}
+
+const members = [man, woman, cat, dog, catWoman];
+const memberProps = ['creature', 'name', 'gender', 'hands', 'legs', 'friends', 'say'];
+members.forEach(member => 
+   print((memberProps 
+      .map(key => keys[key] + member[key]))
+      .join(" ")
+   )
+);
+ 
+/*
+print("Hi, I am a " + man.creature + ';' + man.name + ';' + man.gender + ';' + 
+  man.hands + ';' +man.legs + ';' + man.say + ';' + man.friends);
+print(dog.creature + ';' + dog.name + ';' + dog.gender + ';' + 
+  dog.paws + ';' + dog.say + ';' + dog.friends);*/
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -27,5 +93,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
